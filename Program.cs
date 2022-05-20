867967353d8b28064e6222462aa91bd8cd6f5a85
Трусов Алексей (Trusov_AA) <Trusov_AA@directum.ru>
2022-05-20 13:58:18 +0400

    Load test work to GitHub

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                var isDivideByThree = number % 3 == 0;
+                var isDivideByFive = number % 5 == 0;
+
+                if (isDivideByThree && isDivideByFive)
+                    Console.WriteLine("FizzBuzz");
+                else if (isDivideByThree)
+                    Console.WriteLine("Fizz");
+                else if (isDivideByFive)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(number);
+            }
+        }
+    }
+}
